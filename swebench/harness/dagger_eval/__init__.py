04add92685400064fa044f39afe1246088f0a068
Helder Correia <174525+helderco@users.noreply.github.com>
2025-02-25 22:39:02 -0100

    refactor and enable dagger cloud telemetry with dagger run
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- swebench/harness/dagger_eval/__init__.py
+++ swebench/harness/dagger_eval/__init__.py
@@ -1,1 +1,3 @@
 from swebench.harness.dagger_eval.run_evaluation_dagger import run_instances_dagger
+
+__all__ = ["run_instances_dagger"]
--- swebench/harness/dagger_eval/run_evaluation_dagger.py
+++ swebench/harness/dagger_eval/run_evaluation_dagger.py
@@ -2,27 +2,34 @@
 
 from __future__ import annotations
 
+from collections import Counter
 import functools
 import json
 import logging
-import sys
 import time
 from dataclasses import dataclass
 from logging import Logger
 from pathlib import Path
+from typing import cast
+import sys
 
 import anyio
 import dagger
 from anyio import to_thread
 from dagger import ReturnType, dag
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.trace import get_tracer_provider
+from opentelemetry import trace
+from . import telemetry
+
 
 from swebench.harness.constants import (
     APPLY_PATCH_FAIL,
     APPLY_PATCH_PASS,
     RUN_EVALUATION_LOG_DIR,
 )
-from swebench.harness.constants.constants import SWEbenchInstance
-from swebench.harness.docker_build import setup_logger
+from swebench.harness.constants.constants import KEY_INSTANCE_ID, SWEbenchInstance
+from swebench.harness.docker_build import close_logger, setup_logger
 from swebench.harness.grading import get_eval_report
 from swebench.harness.reporting import make_run_report
 from swebench.harness.test_spec.test_spec import TestSpec, make_test_spec
@@ -35,6 +42,8 @@ REPORT_FILE = "report.json"
 
 
 logging.getLogger("httpx").setLevel(logging.ERROR)
+
+tracer = telemetry.get_tracer()
 
 
 @dataclass
@@ -67,8 +76,60 @@ class Instance:
         return _logger
 
 
-def get_instance_image(test_spec: TestSpec) -> dagger.Container:
-    ctr = (
+async def run_instance_dagger(
+    bench_instance: SWEbenchInstance,
+    prediction: dict[str, str],
+    run_id: str,
+    timeout: int,
+    limiter: anyio.CapacityLimiter,
+):
+    """
+    Run a single instance with the given prediction.
+
+    Args:
+        bench_instance (SWEbenchInstance): SWE-bench instance
+        pred (dict): Prediction w/ model_name_or_path, model_patch, instance_id
+        run_id (str): Run ID
+        timeout (int): Timeout for running tests
+    """
+    async with limiter:
+        test_spec = await to_thread.run_sync(make_test_spec, bench_instance)
+        instance = Instance(run_id, test_spec, prediction)
+        logger = await instance.setup_logger()
+
+        try:
+            with tracer.start_as_current_span(instance.id) as span:
+                with anyio.move_on_after(timeout) as scope:
+                    ctr = await get_instance_image(instance.test_spec)
+                    ctr = await _run_instance_patch(instance, ctr, logger)
+                    ctr = await _run_evaluation_script(instance, ctr, logger)
+                    resolved = await _run_report(instance, logger)
+
+                    if not resolved:
+                        span.set_status(trace.StatusCode.ERROR, "not resolved")
+
+                if scope.cancel_called:
+                    logger.info("Evaluation for model %s timed out", instance.id)
+
+        except EvaluationError as e:
+            logger.info(str(e))
+
+        except Exception:
+            logger.exception(
+                "Error in evaluating module for %s.\nCheck %s for more information",
+                instance.id,
+                instance.log_dir / RUN_LOG_FILE,
+            )
+
+        finally:
+            # TODO: write log file here instead of leaving it open while this is running
+            # to avoid too many open files
+            close_logger(logger)
+
+
+@tracer.start_as_current_span("setup base container")
+async def get_instance_image(test_spec: TestSpec) -> dagger.Container:
+    return await (
         dag.container()
         .from_("ubuntu:22.04")
         .with_exec(["apt", "update"])
@@ -89,110 +150,51 @@ def get_instance_image(test_spec: TestSpec) -> dagger.Container:
                 "locales",
                 "locales-all",
                 "tzdata",
+                "python3.11",
             ]
         )
-        # .with_exec(["wget", "https://repo.anaconda.com/miniconda/Miniconda3-py311_23.11.0-2-Linux-x86_64.sh", "-O", "miniconda.sh"])
         .with_exec(
             [
-                "wget",
-                "https://repo.anaconda.com/miniconda/Miniconda3-py311_23.11.0-2-Linux-aarch64.sh",
-                "-O",
-                "miniconda.sh",
+                "update-alternatives",
+                "--install",
+                "/usr/bin/python",
+                "python",
+                "/usr/bin/python3.11",
+                "7",
             ]
+        )
+        .with_file(
+            "miniconda.sh",
+            dag.http(
+                "https://repo.anaconda.com/miniconda/Miniconda3-py311_23.11.0-2-Linux-aarch64.sh"
+            ),
         )
         .with_exec(["bash", "miniconda.sh", "-b", "-p", "/opt/miniconda3"])
-        .with_exec(
-            ["bash", "-c", "echo 'export PATH=/opt/miniconda3/bin:$PATH' >> ~/.bashrc"]
+        .with_env_variable("PATH", "/opt/miniconda3/bin:$PATH", expand=True)
+        .with_exec(["conda", "init", "--all"])
+        .with_exec(["conda", "config", "--append", "channels", "conda-forge"])
+        .with_exec(["adduser", "--disabled-password", "--gecos", "'dog'", "nonroot"])
+        .with_new_file(
+            "/root/setup_env.sh",
+            test_spec.setup_env_script,
+            permissions=0o755,
         )
-        .with_exec(["/opt/miniconda3/bin/conda", "init", "--all"])
+        .with_new_file("/root/setup_repo.sh", test_spec.install_repo_script)
+        .with_exec(["/root/setup_env.sh"])
         .with_exec(
             [
-                "/opt/miniconda3/bin/conda",
-                "config",
-                "--append",
-                "channels",
-                "conda-forge",
+                "bash",
+                "-c",
+                "echo 'source /opt/miniconda3/etc/profile.d/conda.sh && conda activate testbed' >> /root/.bashrc",
             ]
         )
-        .with_exec(["adduser", "--disabled-password", "--gecos", "'dog'", "nonroot"])
-        .with_workdir("/testbed/")
+        .with_exec(["bash", "/root/setup_repo.sh"])
+        .with_workdir("/testbed")
+        .sync()
     )
-    for command in test_spec.env_script_list:
-        ctr = ctr.with_exec(["bash", "-i", "-c", command])
-    ctr = ctr.with_exec(
-        [
-            "bash",
-            "-c",
-            "echo 'source /opt/miniconda3/etc/profile.d/conda.sh && conda activate testbed' >> /root/.bashrc",
-        ]
-    )
-    for command in test_spec.repo_script_list:
-        ctr = ctr.with_exec(["bash", "-i", "-c", command])
-
-    return ctr
 
 
-async def run_instance_dagger(
-    instance: Instance,
-    timeout: int,
-    limiter: anyio.CapacityLimiter,
-):
-    """
-    Run a single instance with the given prediction.
-
-    Args:
-        test_spec (TestSpec): TestSpec instance
-        pred (dict): Prediction w/ model_name_or_path, model_patch, instance_id
-        run_id (str): Run ID
-        timeout (int): Timeout for running tests
-    """
-    async with limiter:
-        logger = await instance.setup_logger()
-
-        with anyio.move_on_after(timeout) as scope:
-            ctr = get_instance_image(instance.test_spec)
-
-            try:
-                ctr = await _run_instance_patch(
-                    instance,
-                    ctr,
-                    logger,
-                )
-                ctr = await _run_evaluation_script(instance, ctr, logger)
-
-                # Get report from test output
-                logger.info("Grading answer for %s...", instance.id)
-                report = get_eval_report(
-                    test_spec=instance.test_spec,
-                    prediction=instance.pred,
-                    test_log_path=str(instance.log_dir / TEST_LOG_FILE),
-                    include_tests_status=True,
-                )
-                logger.info(
-                    "report: %s\nResult for %s: resolved: %s",
-                    report,
-                    instance.id,
-                    report[instance.id]["resolved"],
-                )
-
-                await anyio.Path(instance.log_dir / REPORT_FILE).write_text(
-                    json.dumps(report, indent=4)
-                )
-
-            except EvaluationError as e:
-                logger.info(str(e))
-
-            except Exception:
-                logger.exception(
-                    "Error in evaluating module for %s.\nCheck %s for more information",
-                    instance.id,
-                    instance.log_dir / RUN_LOG_FILE,
-                )
-
-        if scope.cancel_called:
-            logger.info("Evaluation for model %s timed out", instance.id)
-
-
+@tracer.start_as_current_span("apply patch")
 async def _run_instance_patch(
     instance: Instance,
     ctr: dagger.Container,
@@ -232,32 +234,59 @@ async def _run_instance_patch(
     return patched_ctr
 
 
+@tracer.start_as_current_span("run evaluation script")
 async def _run_evaluation_script(
     instance: Instance,
     ctr: dagger.Container,
     logger: Logger,
 ) -> dagger.Container:
     # Get git diff before running eval script
-    git_diff_output_before = await ctr.with_exec(
-        ["git", "diff"],
-    ).stdout()
+    git_diff_output_before = await ctr.with_exec(["git", "diff"]).stdout()
 
     logger.info("Git diff before:\n%s", git_diff_output_before)
+
+    failures = Counter()
 
     start_time = time.time()
     test_output = ""
     for command in instance.test_spec.eval_script_list:
+        if command in (
+            "source /opt/miniconda3/bin/activate",
+            "conda activate testbed",
+        ):
+            continue
+
         # django hack
         command = command.replace("locale-gen", "locale-gen en_US.UTF-8")
-        ctr = ctr.with_exec(["bash", "-i", "-c", command], expect=ReturnType.ANY)
-        test_output += f"+ {command}\n"
-        test_output += await ctr.stdout()
-        test_output += await ctr.stderr()
+
+        with tracer.start_as_current_span(command) as span:
+            ctr = ctr.with_exec(
+                ["bash", "--login", "-c", command],
+                redirect_stdout="/out",
+                redirect_stderr="/out",
+                expect=ReturnType.ANY,
+            )
+            test_output += f"+ {command}\n"
+            test_output += await ctr.file("/out").contents()
+            code = await ctr.exit_code()
+
+            failures[bool(code)] += 1
+
+            if code:
+                error = trace.StatusCode.ERROR
+                span.set_status(error, f"command failed with exit code {code}")
 
     total_runtime = time.time() - start_time
-    logger.info(f'Test runtime: {total_runtime:_.2f} seconds')
+    logger.info(f"Test runtime: {total_runtime:_.2f} seconds")
 
     await anyio.Path(instance.log_dir / TEST_LOG_FILE).write_text(test_output)
+
+    if failures[True]:
+        trace.get_current_span().set_status(
+            trace.StatusCode.ERROR,
+            f"{failures[True]} command(s) failed (out of {failures.total()})",
+        )
+
     logger.info(
         "Test output for %s written to %s",
         instance.id,
@@ -274,6 +303,35 @@ async def _run_evaluation_script(
         logger.info("Git diff changed after running eval script")
 
     return ctr
+
+
+@tracer.start_as_current_span("generate report")
+async def _run_report(instance: Instance, logger: Logger):
+    # Get report from test output
+    logger.info("Grading answer for %s...", instance.id)
+
+    report = await to_thread.run_sync(
+        functools.partial(
+            get_eval_report,
+            test_spec=instance.test_spec,
+            prediction=instance.pred,
+            test_log_path=str(instance.log_dir / TEST_LOG_FILE),
+            include_tests_status=True,
+        )
+    )
+
+    logger.info(
+        "report: %s\nResult for %s: resolved: %s",
+        report,
+        instance.id,
+        report[instance.id]["resolved"],
+    )
+
+    await anyio.Path(instance.log_dir / REPORT_FILE).write_text(
+        json.dumps(report, indent=4)
+    )
+
+    return report[instance.id]["resolved"]
 
 
 def run_instances_dagger(
@@ -308,10 +366,11 @@ def run_instances_dagger(
         full_dataset,
         run_id,
     )
+    cast(TracerProvider, get_tracer_provider()).shutdown()
 
 
 async def run_instances_dagger_async(
-    predictions: dict,
+    predictions: dict[str, dict[str, str]],
     instances: list[SWEbenchInstance],
     run_id: str,
     max_workers: int,
@@ -328,12 +387,6 @@ async def run_instances_dagger_async(
         timeout (int):
     """
 
-    async def make_instance():
-        for instance in instances:
-            test_spec = await to_thread.run_sync(make_test_spec, instance)
-            prediction = predictions[test_spec.instance_id]
-            yield Instance(run_id, test_spec, prediction)
-
     limiter = anyio.CapacityLimiter(max_workers)
 
     cfg = dagger.Config()
@@ -341,10 +394,12 @@ async def run_instances_dagger_async(
     cfg.console.quiet = True
 
     async with dagger.connection(cfg), anyio.create_task_group() as tg:
-        async for instance in make_instance():
+        for instance in instances:
             tg.start_soon(
                 run_instance_dagger,
                 instance,
+                predictions[instance[KEY_INSTANCE_ID]],
+                run_id,
                 timeout,
                 limiter,
             )
